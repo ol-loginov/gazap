a22ecc18856dcfc09c8fd7c14ea38782c8edff6d
Oleg Loginov <ol.loginov@gmail.com>
2012-06-06 21:17:38 +0400

    tile pictures is not shown

--- src/gazap-domain/src/main/java/gazap/domain/dao/ContributionDao.java
+++ src/gazap-domain/src/main/java/gazap/domain/dao/ContributionDao.java
@@ -1,4 +1,12 @@
 package gazap.domain.dao;
 
+import gazap.domain.entity.*;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ContributionDao extends Dao {
+    List<Contribution> findContributions(Map map, UserProfile author, ContributionDecision actualDecision, Date after);
+
+    ContributionTile loadTile(int id);
 }
--- src/gazap-domain/src/main/java/gazap/domain/dao/impl/ContributionDaoImpl.java
+++ src/gazap-domain/src/main/java/gazap/domain/dao/impl/ContributionDaoImpl.java
@@ -1,8 +1,27 @@
 package gazap.domain.dao.impl;
 
 import gazap.domain.dao.ContributionDao;
+import gazap.domain.entity.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public class ContributionDaoImpl extends DaoImpl implements ContributionDao {
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Contribution> findContributions(Map map, UserProfile author, ContributionDecision actualDecision, Date after) {
+        return getSession().createQuery("from Contribution c where c.map=:map and c.author=:author and c.decision=:decision and c.createdAt>:after order by c.createdAt desc")
+                .setEntity("map", map)
+                .setEntity("author", author)
+                .setParameter("decision", actualDecision)
+                .setParameter("after", after)
+                .list();
+    }
+
+    @Override
+    public ContributionTile loadTile(int id) {
+        return (ContributionTile) getSession().get(ContributionTile.class, id);
+    }
 }
