a498916e2d2ed8f8897de24bcf35772680bff67e
Oleg Loginov <ol.loginov@gmail.com>
2012-07-25 20:51:15 +0400

    rename viewCenter to viewPoint

--- src/gazap-site/src/main/webapp/static/landscape/scripts/api/gazap.ui.map.js
+++ src/gazap-site/src/main/webapp/static/landscape/scripts/api/gazap.ui.map.js
@@ -2,6 +2,11 @@ Gazap.extendNamespace('Ui', function (N, G) {
     var H = new N.DomHelper();
 
     N.PlainMap = function (opts) {
+        G.defineEvents(this, 'finger.hover finger.down finger.up finger.touch finger.leave finger.enter tile.request');
+        G.defineEvents(this, 'drag.start drag.stop drag.move');
+        G.defineEvents(this, 'tile.request');
+        G.defineEvents(this, 'view.point');
+
         var width = Number(opts.width || 0);
         var height = Number(opts.height || 0);
 
@@ -9,32 +14,29 @@ Gazap.extendNamespace('Ui', function (N, G) {
         this.tileMap = opts.map;
         this.tileSize = opts.tileSize;
 
-        this.$container = H.create('div').appendTo(H.byID(opts.container))
+        this.$domRoot = H.create('div').appendTo(H.byID(opts.container))
             .style({position:'relative', overflow:'hidden', background:'red'})
             .addClass('gazap-ui-map-container');
 
         // init eventually
         this.eventListener = new N.PlainMap.EventListener(this);
-        this.$container.bind('mousemove mouseup mousedown mouseenter mouseleave', this.eventListener);
+        this.$domRoot.bind('mousemove mouseup mousedown mouseenter mouseleave', this.eventListener);
 
         // init tiling
-        this.$tileLayer = H.create('div').appendTo(this.$container)
-            .addClass('gazap-ui-map-tile-layer')
+        this.$domTiles = H.create('div').appendTo(this.$domRoot)
+            .addClass('gazap-ui-map-tiles')
             .style({position:'absolute', left:0, top:0, width:0, height:0, overflow:'visible'});
 
         // init custom layering
-        this.layers = [];
-        this.$childrenLayer = H.create('div').appendTo(this.$container)
-            .addClass('gazap-ui-map-children')
+        this.$domLayers = H.create('div').appendTo(this.$domRoot)
+            .addClass('gazap-ui-map-layers')
             .style({position:'absolute', left:0, top:0, width:0, height:0, overflow:'visible'});
+        this.layers = [];
 
         this.viewScale = 1;
-        this.viewCenter = {x:0, y:0};
+        this.viewPoint = {x:0, y:0};
         this.setSize({width:isNaN(width) ? 0 : width, height:isNaN(height) ? 0 : height});
 
-        G.defineEvents(this, 'finger.hover finger.down finger.up finger.touch finger.leave finger.enter tile.request');
-        G.defineEvents(this, 'drag.start drag.stop drag.move');
-        G.defineEvents(this, 'tile.request');
         return this;
     };
 
@@ -42,10 +44,9 @@ Gazap.extendNamespace('Ui', function (N, G) {
         tileSize:null,
         tileServer:null,
 
-        $container:null,
-
-        $tileLayer:null,
-        $childrenLayer:null,
+        $domRoot:null,
+        $domTiles:null,
+        $domLayers:null,
 
         layers:null,
 
@@ -55,13 +56,13 @@ Gazap.extendNamespace('Ui', function (N, G) {
         height:0,
 
         viewScale:0,
-        viewCenter:null,
+        viewPoint:null,
 
         dragNavigatable:true,
 
         createLayer:function (name) {
             var layer = new N.PlainMap.Layer({name:name});
-            layer.$node = H.create('div').appendTo(this.$childrenLayer)
+            layer.$node = H.create('div').appendTo(this.$domLayers)
                 .addClass('gazap-ui-map-child-layer')
                 .style({position:'absolute', left:0, top:0, width:0, height:0, overflow:'visible'});
             this.layers.push(layer);
@@ -72,28 +73,30 @@ Gazap.extendNamespace('Ui', function (N, G) {
             this.width = size.width;
             this.height = size.height;
 
-            this.$container.style({width:this.width, height:this.height});
-            this.$childrenLayer.style({left:this.width / 2, top:this.height / 2});
-            this.$tileLayer.style({left:this.width / 2, top:this.height / 2});
+            this.$domRoot.style({width:this.width, height:this.height});
+            this.$domLayers.style({left:this.width / 2, top:this.height / 2});
+            this.$domTiles.style({left:this.width / 2, top:this.height / 2});
 
-            this._showViewCenter();
+            this.setViewPoint(this.viewPoint);
         },
 
-        _showViewCenter:function () {
-            console.log(this.viewCenter);
-            this.$childrenLayer.style({left:this.width / 2 - this.viewCenter.x, top:this.height / 2 + this.viewCenter.y});
-            this.$tileLayer.style({left:this.width / 2 - this.viewCenter.x, top:this.height / 2 + this.viewCenter.y});
+        setViewPoint:function (pt) {
+            this.viewPoint = pt;
+            this.$domLayers.style({left:this.width / 2 - this.viewPoint.x, top:this.height / 2 + this.viewPoint.y});
+            this.$domTiles.style({left:this.width / 2 - this.viewPoint.x, top:this.height / 2 + this.viewPoint.y});
+            console.log(this.viewPoint);
+            this.trigger('view.point', pt);
         },
 
         mapToClientPoint:function (pt) {
-            var x1 = (pt.x + this.viewCenter.x) / this.viewScale + this.width / 2;
-            var y1 = (pt.y + this.viewCenter.y) / this.viewScale - this.height / 2;
+            var x1 = (pt.x + this.viewPoint.x) / this.viewScale + this.width / 2;
+            var y1 = (pt.y + this.viewPoint.y) / this.viewScale - this.height / 2;
             return {x:x1, y:-y1};
         },
 
         clientToMapPoint:function (pt) {
-            var x2 = (pt.x - this.width / 2) * this.viewScale - this.viewCenter.x;
-            var y2 = (this.height / 2 - pt.y) * this.viewScale - this.viewCenter.y;
+            var x2 = (pt.x - this.width / 2) * this.viewScale - this.viewPoint.x;
+            var y2 = (this.height / 2 - pt.y) * this.viewScale - this.viewPoint.y;
             return {x:x2, y:y2};
         },
 
@@ -123,14 +126,14 @@ Gazap.extendNamespace('Ui', function (N, G) {
             var viewWidth = this.width * this.viewScale;
             var viewHeight = this.height * this.viewScale;
 
-            var boundLeft = this.viewCenter.x - viewWidth / 2;
+            var boundLeft = this.viewPoint.x - viewWidth / 2;
             boundLeft = Math.floor(boundLeft / q) * q;
-            var boundRight = this.viewCenter.x + viewWidth / 2;
+            var boundRight = this.viewPoint.x + viewWidth / 2;
             boundRight = Math.ceil(boundRight / q) * q - q;
 
-            var boundTop = this.viewCenter.y + viewHeight / 2;
+            var boundTop = this.viewPoint.y + viewHeight / 2;
             boundTop = Math.ceil(boundTop / q) * q;
-            var boundBottom = this.viewCenter.y - viewHeight / 2;
+            var boundBottom = this.viewPoint.y - viewHeight / 2;
             boundBottom = Math.floor(boundBottom / q) * q - q;
 
             for (var x = boundLeft; x <= boundRight; x += q) {
@@ -153,7 +156,7 @@ Gazap.extendNamespace('Ui', function (N, G) {
                     .attr('id', id)
                     .attr('alt', '')
                     .style({position:'absolute', border:'none', display:'none'})
-                    .appendTo(this.$tileLayer);
+                    .appendTo(this.$domTiles);
             }
             var currentSrc = img.attr('src');
             if (currentSrc == req.src) {
@@ -187,23 +190,23 @@ Gazap.extendNamespace('Ui', function (N, G) {
         },
 
         dragStart:function (start) {
-            this.viewCenterDrag = {x:this.viewCenter.x, y:this.viewCenter.y};
+            this.viewPointDrag = {x:this.viewPoint.x, y:this.viewPoint.y};
             this.trigger('drag.start', {start:start});
         },
 
         dragMove:function (start, current) {
-            this.viewCenter.x = this.viewCenterDrag.x - (current.x - start.x) - this.viewCenter.x;
-            this.viewCenter.y = this.viewCenterDrag.y - (current.y - start.y) - this.viewCenter.y;
-            this._showViewCenter();
+            var x = this.viewPointDrag.x - (current.x - start.x) - this.viewPoint.x;
+            var y = this.viewPointDrag.y - (current.y - start.y) - this.viewPoint.y;
+            this.setViewPoint({x:x, y:y});
 
             this.trigger('drag.move', {start:start, current:current});
         },
 
         dragStop:function (start, current) {
-            this.viewCenter.x = this.viewCenterDrag.x - (current.x - start.x) - this.viewCenter.x;
-            this.viewCenter.y = this.viewCenterDrag.y - (current.y - start.y) - this.viewCenter.y;
-            this.viewCenterDrag = null;
-            this._showViewCenter();
+            var x = this.viewPointDrag.x - (current.x - start.x) - this.viewPoint.x;
+            var y = this.viewPointDrag.y - (current.y - start.y) - this.viewPoint.y;
+            this.viewPointDrag = null;
+            this.setViewPoint({x:x, y:y});
 
             this.trigger('drag.stop', {start:start, current:current});
         }
