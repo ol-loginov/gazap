70c9485b64b0978708cbc9212c1c50c8a5970d1d
Oleg Loginov <ol.loginov@gmail.com>
2012-10-04 18:38:20 +0400

    sniper design

--- src/waypalm-site/src/main/java/waypalm/site/web/extensions/ViewMetaExtender.java
+++ src/waypalm-site/src/main/java/waypalm/site/web/extensions/ViewMetaExtender.java
@@ -1,8 +1,5 @@
 package waypalm.site.web.extensions;
 
-import waypalm.site.web.views.errors.AuthError;
-import waypalm.site.web.views.errors.HttpError;
-import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.WebRequest;
 
 public class ViewMetaExtender extends Extender<ViewMetaExtension> {
@@ -11,27 +8,6 @@ public class ViewMetaExtender extends Extender<ViewMetaExtension> {
         if (content == null) {
             return null;
         }
-
-        if (content instanceof AuthError) {
-            extension = instantiateIfNull(extension, ViewMetaExtension.class);
-            setHeader((AuthError) content, extension);
-        } else if (content instanceof HttpError) {
-            extension = instantiateIfNull(extension, ViewMetaExtension.class);
-            setHeader((HttpError) content, extension);
-        }
-
-        return extension;
-    }
-
-    private void setHeader(AuthError view, ViewMetaExtension module) {
-        if (StringUtils.hasText(view.getErrorCode())) {
-            module.setTitleKey("auth." + view.getErrorCode() + ".pageTitle");
-        }
-    }
-
-    private void setHeader(HttpError view, ViewMetaExtension module) {
-        if (StringUtils.hasText(view.getHttp())) {
-            module.setTitleKey("http." + view.getHttp() + ".pageTitle");
-        }
+        return instantiateIfNull(extension, ViewMetaExtension.class);
     }
 }
--- src/waypalm-site/src/main/java/waypalm/site/web/extensions/ViewMetaExtension.java
+++ src/waypalm-site/src/main/java/waypalm/site/web/extensions/ViewMetaExtension.java
@@ -1,22 +1,4 @@
 package waypalm.site.web.extensions;
 
 public class ViewMetaExtension {
-    private String titleKey;
-    private String title;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getTitleKey() {
-        return titleKey;
-    }
-
-    public void setTitleKey(String titleKey) {
-        this.titleKey = titleKey;
-    }
 }
