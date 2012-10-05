f1db1121acd3edda575e94d05a9fd40433aef562
Oleg Loginov <ol.loginov@gmail.com>
2012-10-05 20:31:48 +0400

    gloss

--- src/waypalm-site/src/main/webapp/static/theme/Makefile.sh
+++ src/waypalm-site/src/main/webapp/static/theme/Makefile.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 # make css
-mkdir -p ./dist/css
-lessc --verbose ./less/layout.less > ./dist/waypalm.css
-lessc --compress --verbose ./less/layout.less > ./dist/waypalm.min.css
-
-# make javascripts
-for file in `find ./scripts -name *.js`
-do
-  filefolder=${file%/*.js}
-  filefolder=${filefolder#./}
-  filename=${file##*/}
-  mkdir -p dist/$filefolder
-
-  uglifyjs --output dist/$filefolder/$filename $file
-done
-
+mkdir -p ./dist
+lessc --verbose ./less/layout.less > ./dist/theme.css
+lessc --compress --verbose ./less/layout.less > ./dist/theme.min.css
