bb873ed8aedd68506b66f85f43bb914360d3dc59
Oleg Loginov <ol.loginov@gmail.com>
2013-05-28 21:32:32 +0400

    lib changes

--- src/waypalm-common-web/src/main/java/org/springframework/context/support/ReloadableResourceBundleMessageSourceWithLocaleChoice.java
+++ src/waypalm-common-web/src/main/java/org/springframework/context/support/ReloadableResourceBundleMessageSourceWithLocaleChoice.java
@@ -1,13 +1,15 @@
 package org.springframework.context.support;
 
-import com.iserv2.commons.text.ExtendedMessageFormatUtil;
+import com.iserv2.commons.text.ExtendedMessageFormats;
 import org.apache.commons.lang.text.ExtendedMessageFormat;
 
 import java.util.Locale;
 
 public class ReloadableResourceBundleMessageSourceWithLocaleChoice extends ReloadableResourceBundleMessageSource {
+    private final ExtendedMessageFormats messageFormats = new ExtendedMessageFormats();
+
     @Override
     protected ExtendedMessageFormat createMessageFormat(String msg, Locale locale) {
-        return ExtendedMessageFormatUtil.createMessageFormat(msg, locale);
+        return messageFormats.createMessageFormat(msg, locale);
     }
 }
